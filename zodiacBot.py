4b4e2d07b3528741d83fd14cac1e043f88ff383e
olkhovich <olkhovichs@icloud.com>
2020-10-30 00:36:47 +0300

    add two buttons

--- zodiacBot.py
+++ zodiacBot.py
@@ -1,1 +1,28 @@
-import telebot; bot = telebot.TeleBot('1286203012:AAE4oeJueqbAbCcUjhSn0Vy-DQz0V1CRb_s')
+import telebot; bot = telebot.TeleBot('1184822377:AAFI7WcS2XEEj00lKOUXbC-tDHIuEY4Roz8')
+
+from telebot import types
+
+
+@bot.message_handler(content_types=['text'])
+def sendText(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, я сейчас расскажу тебе про твой знак зодиака.")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши 'Привет'")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши ''/help'")
+
+
+#zodiacSign = str(input("{blue}Введите свой знак зодиака: {endcolor}".format(blue="\033[96m", endcolor="\033[0m"))))
+
+keyboard = types.InlineKeyboardMarkup()
+keyTwins = types.InlineKeyboardButton(text = 'Близнецы ♊', callBack_data = 'zodiacSign')
+keyboard.add(keyTwins)
+
+keyboard = types.InlineKeyboardMarkup()
+keyTaurus = types.InlineKeyboardButton(text = 'Телец ♉', callBack_data = 'zodiacSign')
+keyboard.add(keyTaurus)
+
+bot.send_message(message.from_user.id, text = 'Выбери свой знак зодиака', reply_murkup = keyboard)
+
+bot.polling()
